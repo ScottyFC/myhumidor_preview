b4687b7e217824c6e4d445f1c9339cff1cb1c129
ScottyFC <sjeffery@freecast.com>
2026-07-15 13:43:51 -0400

    sync

--- .open-next/cloudflare/init.js
+++ .open-next/cloudflare/init.js
@@ -47,7 +47,7 @@ function initRuntime() {
   };
   Object.assign(globalThis, {
     Request: CustomRequest,
-    __BUILD_TIMESTAMP_MS__: 1784133009544,
+    __BUILD_TIMESTAMP_MS__: 1784137378979,
     __NEXT_BASE_PATH__: "",
     __ASSETS_RUN_WORKER_FIRST__: false,
     __TRAILING_SLASH__: false,
